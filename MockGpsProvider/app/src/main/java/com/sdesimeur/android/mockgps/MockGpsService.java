0b41a583518192b0236a70fd5267f664107a5db3
sdesimeur <pro@sdesimeur.com>
2016-03-28 21:43:23 +0200

    Send every 2s newLocation

--- MockGpsProvider/app/src/main/java/com/sdesimeur/android/mockgps/MockGpsService.java
+++ MockGpsProvider/app/src/main/java/com/sdesimeur/android/mockgps/MockGpsService.java
@@ -1,11 +1,13 @@
 package com.sdesimeur.android.mockgps;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -23,6 +25,7 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -36,13 +39,13 @@ public class MockGpsService extends Service implements LocationListener {
 	private static final int SHORTTIMEOUT4HTMLREQUEST = 2000;
 	//private static final int WAIT = 900;
 	private String pattern = null;
-	private double angle=0;
+	private double angle = 0;
 	private Pattern compiledPattern = null;
-	private double lat=49.59266;
-	private double lon=3.65649;
+	private double lat = 49.59266;
+	private double lon = 3.65649;
 	//private int nbretrywithcancel;
 	private boolean mustBeAborted;
-	private boolean allreadySend=false;
+	private boolean allreadySend = false;
 	//private boolean mustWait = true;
 	//private static final String MOCKGPSTHREAD = "MockGpsThread";
 	private String serverString = MockGpsProviderActivity.SERVERS.get(0);
@@ -50,7 +53,7 @@ public class MockGpsService extends Service implements LocationListener {
 	private final IBinder mBinder = new LocalBinder();
 	//private boolean isInterrupted = false;
 	private Timer timer = null;
-	private Timer timer1 = null;
+	//private Timer timer1 = null;
 
 	public class LocalBinder extends Binder {
 		MockGpsService getService() {
@@ -59,147 +62,139 @@ public class MockGpsService extends Service implements LocationListener {
 	}
 
 	public MockGpsService() {
-		this.pattern="lat\\s*\\=\\s*(\\-?\\d*\\.?\\d*)\\s*,\\s*lon\\s*\\=\\s*(\\-?\\d*\\.?\\d*)";
+		this.pattern = "lat\\s*\\=\\s*(\\-?\\d*\\.?\\d*)\\s*,\\s*lon\\s*\\=\\s*(\\-?\\d*\\.?\\d*)";
 		this.compiledPattern = Pattern.compile(this.pattern);
 		if (android.os.Build.VERSION.SDK_INT > 9) {
-		    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-    		StrictMode.setThreadPolicy(policy);
+			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+			StrictMode.setThreadPolicy(policy);
 		}
 	}
-@TargetApi(17)
+
+	@TargetApi(17)
+	private void sendNewLocation() {
+		newLocation = new Location(LocationManager.GPS_PROVIDER);
+		newLocation.setLatitude(lat);
+		newLocation.setLongitude(lon);
+		newLocation.setAltitude(100);
+		newLocation.setBearing((float) angle);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+			newLocation.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+		}
+		newLocation.setSpeed(50);
+		newLocation.setAccuracy(5);
+		newLocation.setTime(System.currentTimeMillis());
+		Method method;
+		try {
+			method = Location.class.getMethod("makeComplete", new Class[0]);
+			if (method != null) {
+				try {
+					method.invoke(newLocation, new Object[0]);
+				} catch (Exception exception) {
+				}
+			}
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		locationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, newLocation);
+	}
+
 	private void startProvider() {
 		if (timer != null) stopProvider();
 		locationManager.addTestProvider(
-                LocationManager.GPS_PROVIDER,
-                "requiresNetwork" == "",
-                "requiresSatellite" == "",
-                "requiresCell" == "",
-                "hasMonetaryCost" == "",
-                "supportsAltitude" == "",
-                "supportsSpeed" == "",
-                "supportsBearing" == "",
-                Criteria.POWER_HIGH,
-                Criteria.ACCURACY_FINE
-        );
+				LocationManager.GPS_PROVIDER,
+				"requiresNetwork" == "",
+				"requiresSatellite" == "",
+				"requiresCell" == "",
+				"hasMonetaryCost" == "",
+				"supportsAltitude" == "",
+				"supportsSpeed" == "",
+				"supportsBearing" == "",
+				Criteria.POWER_HIGH,
+				Criteria.ACCURACY_FINE
+		);
 		locationManager.setTestProviderStatus(LocationManager.GPS_PROVIDER, LocationProvider.AVAILABLE, null, System.currentTimeMillis());
 		locationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
-		newLocation = new Location(LocationManager.GPS_PROVIDER);
-        newLocation.setLatitude(lat);
-        newLocation.setLongitude(lon);
-        newLocation.setAltitude(100);
-        newLocation.setBearing((float) angle);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            newLocation.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
-        }
-        newLocation.setSpeed(50);
-        newLocation.setAccuracy(5);
-        newLocation.setTime(System.currentTimeMillis());
-		locationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, newLocation);
-
-			mustBeAborted = false;
-			TimerTask task = new TimerTask() {
-				@Override
-				public void run() {
-					String readString = "";
-					URL url = null;
-					InputStreamReader isr = null;
-					BufferedReader in = null;
-					try {
-						if (serverString!="") {
-							url = new URL(serverString + "/tracking/get.php");
-							isr = new InputStreamReader(url.openStream());
-							in = new BufferedReader(isr);
-							String inputLine;
-							while ((inputLine = in.readLine()) != null) {
-								readString += inputLine;
-								if (mustBeAborted) break;
-							}
-							in.close();
+        /*timer1 = new Timer();
+        TimerTask task1 = new TimerTask() {
+            @Override
+            public void run() {
+                sendNewLocation();
+            }
+        };
+        timer1.scheduleAtFixedRate(task1, 0, MockGpsService.LONGTIMEOUT4SEND);*/
+		mustBeAborted = false;
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				String readString = "";
+				URL url = null;
+				InputStreamReader isr = null;
+				BufferedReader in = null;
+				try {
+					if (serverString != "") {
+						url = new URL(serverString + "/tracking/get.php");
+						isr = new InputStreamReader(url.openStream());
+						in = new BufferedReader(isr);
+						String inputLine;
+						while ((inputLine = in.readLine()) != null) {
+							readString += inputLine;
+							if (mustBeAborted) break;
 						}
-					} catch (IOException e) {
-						e.printStackTrace();
+						in.close();
 					}
-					if ("" != readString) {
-						Matcher m = compiledPattern.matcher(readString);
-						if (m.find()) {
-							lat = Double.parseDouble(m.group(1));
-							lon = Double.parseDouble(m.group(2));
-							if ((newLocation == null) || (lat != newLocation.getLatitude()) || (lon != newLocation.getLongitude())) {
-								if (newLocation != null) {
-									double x = lat - newLocation.getLatitude();
-									double y = lon - newLocation.getLongitude();
-									angle = Math.toDegrees(Math.atan2(y, x));
-								} else {
-									angle = 0;
-								}
-								newLocation = new Location(LocationManager.GPS_PROVIDER);
-                                newLocation.setLatitude(lat);
-                                newLocation.setLongitude(lon);
-								newLocation.setAltitude(100);
-                                newLocation.setBearing((float) angle);
-                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                                    newLocation.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
-                                }
-                                newLocation.setSpeed(50);
-                                newLocation.setAccuracy(5);
-								newLocation.setTime(System.currentTimeMillis());
-								//newLocation.setProvider(LocationManager.GPS_PROVIDER);
-								//newLocation.setExtras(null);
-								/*Method method;
-								try {
-									method = Location.class.getMethod("makeComplete", new Class[0]);
-									if (method != null)
-									{
-										try
-										{
-											method.invoke(newLocation, new Object[0]);
-										}
-										catch (Exception exception) { }
-									}
-								} catch (NoSuchMethodException e) {
-									// TODO Auto-generated catch block
-									e.printStackTrace();
-								}*/
-								locationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, newLocation);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				if ("" != readString) {
+					Matcher m = compiledPattern.matcher(readString);
+					if (m.find()) {
+						lat = Double.parseDouble(m.group(1));
+						lon = Double.parseDouble(m.group(2));
+						if ((newLocation == null) || (lat != newLocation.getLatitude()) || (lon != newLocation.getLongitude())) {
+							if (newLocation != null) {
+								double x = lat - newLocation.getLatitude();
+								double y = lon - newLocation.getLongitude();
+								angle = Math.toDegrees(Math.atan2(y, x));
+							} else {
+								angle = 0;
 							}
 						}
 					}
 				}
-			};
-			timer = new Timer();
-			timer.scheduleAtFixedRate(task,0, MockGpsService.SHORTTIMEOUT4HTMLREQUEST);
-			timer1 = new Timer();
-			TimerTask task1 = new TimerTask() {
-				@Override
-				public void run() {
-					newLocation = new Location(LocationManager.GPS_PROVIDER);
-                    newLocation.setLatitude(lat);
-                    newLocation.setLongitude(lon);
-                    newLocation.setAltitude(100);
-                    newLocation.setBearing((float) angle);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                        newLocation.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
-                    }
-                    newLocation.setSpeed(50);
-                    newLocation.setAccuracy(5);
-                    newLocation.setTime(System.currentTimeMillis());
-					locationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, newLocation);
-				}
-			};
-			timer1.scheduleAtFixedRate(task1,0,MockGpsService.LONGTIMEOUT4SEND);
+                sendNewLocation();
+			}
+		};
+		timer = new Timer();
+		timer.scheduleAtFixedRate(task, 0, MockGpsService.SHORTTIMEOUT4HTMLREQUEST);
 	}
 
 	private void stopProvider() {
 		mustBeAborted = true;
 		timer.cancel();
 		timer = null;
+        //timer1.cancel();
+        //timer1=null;
 		locationManager.removeTestProvider(LocationManager.GPS_PROVIDER);
 	}
+
 	@Override
-    public void onCreate() {
-		locationManager	= (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	public void onCreate() {
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+			if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    Activity#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                return;
+            }
+		}
 		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 2, this);
-    }
+	}
 
 	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 	@Override
