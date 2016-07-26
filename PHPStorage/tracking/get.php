6733d5d95c87d06d74d73ee198f870099f324ea0
sdesimeur <pro@sdesimeur.com>
2016-07-26 16:30:10 +0200

    debug PHPStorage/tracking/get.php

--- PHPStorage/tracking/get.php
+++ PHPStorage/tracking/get.php
@@ -5,7 +5,10 @@ $txt=file_get_contents($file);
 Header ("Content-type: text/plain; charset=utf-8");
 $tab=explode(",",$txt);
 $tab1=str_replace(array("lat=","lon="),"",$tab);
-$type=$_GET["type"];
+$type="";
+if (isset($_GET["type"])) {
+    $type=$_GET["type"];
+    }
 if ($type=="lat") {
 	echo $tab1[0];
 } elseif ($type=="lon") {
