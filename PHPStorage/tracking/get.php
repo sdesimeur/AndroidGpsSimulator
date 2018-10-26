9c1beb4883a8bd65a3aae2d52d4a64b31ef772bf
SDesimeur <pro@sdesimeur.com>
2018-10-26 10:06:40 +0200

    add altitude\ncreate a flight simulator

--- PHPStorage/tracking/get.php
+++ PHPStorage/tracking/get.php
@@ -1,19 +1,17 @@
 <?php
-//$file="/tmp/coord.txt";
 $file="./files/coord.txt";
 $txt=file_get_contents($file);
 Header ("Content-type: text/plain; charset=utf-8");
 $tab=explode(",",$txt);
-$tab1=str_replace(array("lat=","lon="),"",$tab);
-$type="";
-if (isset($_GET["type"])) {
-    $type=$_GET["type"];
-    }
+$tab1=str_replace(array("lat=","lon=","alt="),"",$tab);
+$type=$_GET["type"];
 if ($type=="lat") {
 	echo $tab1[0];
 } elseif ($type=="lon") {
 	echo $tab1[1];
+} elseif ($type=="alt") {
+	echo $tab1[2];
 } else {
-		echo $txt;
+	echo ";" . $txt . ";";
 }
 ?>
--- PHPStorage/tracking/put.php
+++ PHPStorage/tracking/put.php
@@ -1,8 +1,13 @@
 <?php
 $lon=$_GET['lon'];
 $lat=$_GET['lat'];
+$alt=$_GET['alt'];
 $file="./files/coord.txt";
-//$file="/tmp/coord.txt";
-$txt="lat=".$lat . ",lon=" . $lon;
-file_put_contents($file,$txt,LOCK_EX);
+$txt="lat=".$lat . ",lon=" . $lon . ",alt=" . $alt;
+$size = file_put_contents($file,$txt,LOCK_EX);
+//$binarydata = sprintf("%08X%08X%08X", $lat, $lon, $alt);
+$binarydata = pack("NNN", $lat, $lon, $alt);
+//$fileHex="./files/coord.bin.txt";
+$fileHex="./files/coord.bin";
+$size = file_put_contents($fileHex,$binarydata,LOCK_EX);
 ?>
