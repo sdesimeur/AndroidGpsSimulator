34bc9de9499b84fbcfa1c61f1c4ee9af73f409c9
SDesimeur <pro@sdesimeur.com>
2018-11-21 12:09:31 +0100

    put.php use tmp file then move it

--- MapMockGpsPHP/tracking/put.php
+++ MapMockGpsPHP/tracking/put.php
@@ -2,15 +2,17 @@
 $lon=$_GET['lon'];
 $lat=$_GET['lat'];
 $alt=$_GET['alt'];
-$file="./files/coord.txt";
+$file="./files/coord_tmp.txt";
 $txt="lat=".$lat . ",lon=" . $lon . ",alt=" . $alt;
 $size = file_put_contents($file,$txt,LOCK_EX);
+rename($file, "./files/coord.txt");
 //$binarydata = sprintf("%08X%08X%08X", $lat, $lon, $alt);
 $lat = round($lat*10000000);
 $lon = round($lon*10000000);
 $alt = round($alt);
 $binarydata = pack("NNN", $lat, $lon, $alt);
 //$fileHex="./files/coord.bin.txt";
-$fileHex="./files/coord.bin";
+$fileHex="./files/coord_tmp.bin";
 $size = file_put_contents($fileHex,$binarydata,LOCK_EX);
+rename($fileHex, "./files/coord.bin");
 ?>
