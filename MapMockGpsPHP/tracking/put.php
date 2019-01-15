4d56ab5e8ba7feee2382d4d6cd94b90ed92b230e
SDesimeur <pro@sdesimeur.com>
2019-01-15 10:45:28 +0100

    bug with time/date

--- MapMockGpsPHP/tracking/put.php
+++ MapMockGpsPHP/tracking/put.php
@@ -11,7 +11,7 @@ rename($file, "./files/coord.txt");
 $lat = round($lat*10000000);
 $lon = round($lon*10000000);
 $alt = round($alt);
-$binarydata = pack("NNN", $lat, $lon, $alt);
+$binarydata = pack("NNNN", $lat, $lon, $alt, $mytime);
 //$fileHex="./files/coord.bin.txt";
 $fileHex="./files/coord_tmp.bin";
 $size = file_put_contents($fileHex,$binarydata,LOCK_EX);
