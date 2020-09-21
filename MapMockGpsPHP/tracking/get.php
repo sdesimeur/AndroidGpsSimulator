e1a4c4d885a7a814e53b9bc147e73adda9e7d6ec
sam <sam@Ubuntu.20.04>
2020-09-21 13:55:49 +0200

    replace lon by lng

--- MapMockGpsPHP/tracking/get.php
+++ MapMockGpsPHP/tracking/get.php
@@ -3,11 +3,11 @@ $file="./files/coord.txt";
 $txt=file_get_contents($file);
 Header ("Content-type: text/plain; charset=utf-8");
 $tab=explode(",",$txt);
-$tab1=str_replace(array("lat=","lon=","alt=","time="),"",$tab);
+$tab1=str_replace(array("lat=","lng=","alt=","time="),"",$tab);
 $type=$_GET["type"];
 if ($type=="lat") {
 	echo $tab1[0];
-} elseif ($type=="lon") {
+} elseif ($type=="lng") {
 	echo $tab1[1];
 } elseif ($type=="alt") {
 	echo $tab1[2];
--- MapMockGpsPHP/tracking/put.php
+++ MapMockGpsPHP/tracking/put.php
@@ -1,19 +1,19 @@
 <?php
-$lon=$_GET['lon'];
+$lng=$_GET['lng'];
 $lat=$_GET['lat'];
 $alt=$_GET['alt'];
 $spd=$_GET['spd'];
 $hdg=$_GET['hdg'];
 $mytime=$_GET['time'];
 $file="./files/coord_tmp.txt";
-$txt="lat=".$lat . ",lon=" . $lon . ",alt=" . $alt . ",time=". $mytime . ",spd=" . $spd . ",hdg=" . $hdg;
+$txt="lat=".$lat . ",lng=" . $lng . ",alt=" . $alt . ",time=". $mytime . ",spd=" . $spd . ",hdg=" . $hdg;
 $size = file_put_contents($file,$txt,LOCK_EX);
 rename($file, "./files/coord.txt");
-//$binarydata = sprintf("%08X%08X%08X%08X", $lat, $lon, $alt, $mytime);
+//$binarydata = sprintf("%08X%08X%08X%08X", $lat, $lng, $alt, $mytime);
 $lat = round($lat*10000000);
-$lon = round($lon*10000000);
+$lng = round($lng*10000000);
 $alt = round($alt);
-$binarydata = pack("NNNN", $lat, $lon, $alt, $mytime);
+$binarydata = pack("NNNN", $lat, $lng, $alt, $mytime);
 //$fileHex="./files/coord.bin.txt";
 $fileHex="./files/coord_tmp.bin";
 $size = file_put_contents($fileHex,$binarydata,LOCK_EX);
