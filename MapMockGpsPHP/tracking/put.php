82a40247dea504ecd6acef28e997122b7b29b62a
Desimeur <samuel.desimeur@syride.com>
2020-09-21 12:16:20 +0200

    add speed

--- MapMockGpsPHP/tracking/put.php
+++ MapMockGpsPHP/tracking/put.php
@@ -2,9 +2,10 @@
 $lon=$_GET['lon'];
 $lat=$_GET['lat'];
 $alt=$_GET['alt'];
+$spd=$_GET['spd'];
 $mytime=$_GET['time'];
 $file="./files/coord_tmp.txt";
-$txt="lat=".$lat . ",lon=" . $lon . ",alt=" . $alt . ",time=". $mytime;
+$txt="lat=".$lat . ",lon=" . $lon . ",alt=" . $alt . ",time=". $mytime . ",spd=" . $spd;
 $size = file_put_contents($file,$txt,LOCK_EX);
 rename($file, "./files/coord.txt");
 //$binarydata = sprintf("%08X%08X%08X%08X", $lat, $lon, $alt, $mytime);
